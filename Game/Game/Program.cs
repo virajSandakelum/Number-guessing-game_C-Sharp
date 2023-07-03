9a72856ec2ccc2707129e3305ade7af878c9ce13
virajSandakelum <2020cs011@stu.ucsc.cmb.ac.lk>
2023-07-03 11:41:15 +0530

    complete game

--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -1,26 +1,21 @@
 ﻿internal class Program
 {
-    private static void Main(string[] args)
+    static public void game(string userName)
     {
-        string appName = "Number Guesser";
-        string appVersion = "1.0.0";
-        string appAuthor = "Viraj Sandakleum";
 
-        Console.ForegroundColor = ConsoleColor.Cyan;
-
-        Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+        Console.WriteLine("Enter the number between 1-10");
         Console.WriteLine();
 
-        Console.ResetColor();
 
-        Console.Write("What is your name? ");
-        string userName = Console.ReadLine();
-
-        Console.WriteLine("Hello {0}, Let's play a game...", userName);
-        Console.WriteLine();
-
-        int correctNumber = 7;
         int guestNumber = 0;
+        string playAgain = "Y";
+
+        Random random = new Random();
+
+        int correctNumber = random.Next(1, 10);
+
+        Console.WriteLine(correctNumber);
+
 
         do
         {
@@ -45,17 +40,60 @@
                 }
                 else
                 {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("You WON!");
+                    while (playAgain != "N")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("{0},You WON!", userName);
+
+                        Console.Write("Play Again? [Y or N] ");
+                        playAgain = Console.ReadLine().ToUpper();
+                        Console.WriteLine();
+
+                        if (playAgain == "Y")
+                        {
+                            game(userName);
+                        }
+                        if (playAgain == "N")
+                        {
+                            return;
+                        }
+
+                        Console.ResetColor();
+                    }
                 }
 
                 Console.ResetColor();
                 Console.WriteLine();
             }
 
-
         } while (guestNumber != correctNumber);
 
         Console.ReadLine();
     }
+
+
+    private static void Main(string[] args)
+    {
+
+        string appName = "Number Guesser";
+        string appVersion = "1.0.0";
+        string appAuthor = "Viraj Sandakleum";
+
+        Console.ForegroundColor = ConsoleColor.Cyan;
+
+        Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
+        Console.WriteLine();
+
+        Console.ResetColor();
+
+        Console.Write("What is your name? ");
+        string userName = Console.ReadLine();
+
+        Console.WriteLine("Hello {0}, Let's play a game...", userName);
+        Console.WriteLine();
+
+        game(userName);
+    }
+
+
 }
